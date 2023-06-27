fda8af4d09d9fcfef18a029f3ef74039cb455bae
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-06-27 07:01:08 -0600

    Various handlers and validators complete

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,107 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+const validateDishExists = (req, res, next) => {
+    const {dishesId} = req.params
+    const index = dishes.findIndex((dish)=> dish.id === dishesId)
+
+    if(index < 0) {
+        next({
+            status: 404,
+            message: `No dish with the id ${dishesId}`
+        })
+    }else{
+        res.locals.index = index
+        res.locals.dish = dishes[index]
+        next()
+    }
+}
+
+const validator = (field) => {
+    return function(req, res, next) {
+        if(req.body.data[field]){
+            next()
+        }else{
+            next({
+                status: 400,
+                message: `You forgot the ${field} field`
+            })
+        }
+    }
+}
+
+const validatePriceIsNumeric = (req, res, next) => {
+    let {price} = req.body.data
+    if(typeof price === "number" && price > 0){
+        next()
+    }else{
+        next({
+            status: 400,
+            message: `price must be a number greater than 0`
+        })
+    }
+}
+
+const list = (req, res, next) => {
+    res.send({ data: dishes })
+}
+
+const read = (req, res, next) => {
+    res.send({ data: res.locals.dish })
+}
+
+const update = (req, res, next) => {
+    const {name, description, price, image_url} = req.body.data
+    const {index, dish} = res.locals
+    const updatedDish = {
+        ...dish,
+        name,
+        description,
+        price,
+        image_url
+    }
+    dishes[index] = updatedDish
+    res.send({ data: updatedDish })
+}
+
+const create = (req, res, next) => {
+    const newId = nextId()
+    const {name, description, price, image_url} = req.body.data
+
+    let newDish = {
+        id: newId,
+        name: name,
+        description: description,
+        price: price,
+        image_url: image_url
+    }
+
+    dishes.push(newDish)
+    res.status(201).send({ data: newDish })
+}
+
+
+
+module.exports = {
+    list,
+    read: [validateDishExists, read],
+    update: [
+        validateDishExists, 
+        validator("id"), 
+        validator("name"), 
+        validator("description"),
+        validator("price"),
+        validator("image_url"),
+        validatePriceIsNumeric,
+        update
+    ],
+    create: [
+        validator("name"), 
+        validator("description"),
+        validator("price"),
+        validator("image_url"),
+        validatePriceIsNumeric,
+        create
+    ]
+}
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,105 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+const validateOrderExists = (req, res, next) => {
+    const {orderId} = req.params
+    const index = orders.findIndex((order)=> order.id === orderId)
+
+    if(index < 0) {
+        next({
+            status: 404,
+            message: `No order with the id ${orderId}`
+        })
+    }else{
+        res.locals.index = index
+        res.locals.order = orders[index]
+        next()
+    }
+}
+
+const validator = (field) => {
+    return function(req, res, next) {
+        if(req.body.data[field]){
+            next()
+        }else{
+            next({
+                status: 400,
+                message: `You forgot the ${field} field`
+            })
+        }
+    }
+}
+
+const validateDishQuantityIsAnInteger = (req, res, next) => {
+    let {quantity} = req.body.data
+    if(typeof quantity === "number" && quantity > 0){
+        next()
+    }else{
+        next({
+            status: 400,
+            message: "quantity must be an integar greater than 0"
+        })
+    }
+}
+
+const create = (req, res, next) => {
+    const newId = nextId()
+    const {deliverTo, mobileNumber, status, dishes} = req.body.data
+
+    let newOrder = {
+        id: newId,
+        deliverTo: deliverTo,
+        mobileNumber: mobileNumber,
+        status: status,
+        dishes: dishes
+    }
+
+    orders.push(newOrder)
+    res.status(201).send({ data: newOrder })
+}
+
+const update = (req, res, next) => {
+    const {deliverTo, mobileNumber, status, dishes} = req.body.data
+    const {index, order} = res.locals
+    const updatedOrder = {
+        ...order,
+        deliverTo,
+        mobileNumber,
+        status,
+        dishes
+    }
+    orders[index] = updatedOrder
+    res.send({ data: updatedOrder })
+}
+
+const list = (req, res, next) => {
+    res.send({ data: orders})
+}
+
+const read = (req, res, next) => {
+    res.send({ data: res.locals.order })
+}
+
+
+module.exports = {
+    list,
+    read: [validateOrderExists, read],
+    create: [
+        validator("deliverTo"), 
+        validator("mobileNumber"),
+        validator("status"),
+        validator("dishes"),
+        validateDishQuantityIsAnInteger,
+        create
+    ],
+    update: [
+        validateOrderExists,
+        validator("deliverTo"), 
+        validator("mobileNumber"),
+        validator("status"),
+        validator("dishes"),
+        validateDishQuantityIsAnInteger,
+        update
+    ]
+}
